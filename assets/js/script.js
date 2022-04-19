a77edf3d43fb28ec302b7e0066cc39fd9f32da8d
riskii24 <100845578+riskii24@users.noreply.github.com>
2022-04-19 17:20:41 +0700

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,81 @@
+(function() {
+  "use strict";
+
+  let swiper = new Swiper(".about-slider", {
+    spaceBetween: 30,
+    effect: "fade",
+    loop: true,
+    mousewheel: {
+      invert: false
+    },
+    // autoHeight: true,
+    pagination: {
+      el: ".about-slider-pagination",
+      clickable: true
+    }
+  });
+
+  let navbar = document.getElementsByTagName('nav')[0];
+  window.addEventListener('scroll', function(){
+    if (window.scrollY > 1) {
+      navbar.classList.replace('bg-transparent', 'nav-color');
+    } else if (window.scrollY <= 0) {
+      navbar.classList.replace('nav-color', 'bg-transparent');
+    }
+  })
+
+  $('#carousel1').owlCarousel({
+    autoplay: true,
+    loop: true,
+    responsiveClass: true,
+    autoHeight: true,
+    autoplayTimeout: 3000,
+    smartSpeed: 800,
+    nav: true,
+    responsive: {
+      0: {
+        items: 1,
+        margin:10
+      },
+
+      600: {
+        items: 2.2,
+        margin:10
+      },
+
+      1024: {
+        items: 3.2,
+        margin:10
+      }
+    }
+  })
+
+  $('#carousel2').owlCarousel({
+    autoplay: true,
+    loop: true,
+    responsiveClass: true,
+    autoHeight: true,
+    autoplayTimeout: 3000,
+    smartSpeed: 800,
+    nav: true,
+    responsive: {
+      0: {
+        items: 1,
+        margin:10
+      },
+
+      600: {
+        items: 2.2,
+        margin:10
+      },
+
+      1024: {
+        items: 3.2,
+        margin:10
+      }
+    }
+  })
+  
+})()
+
+    
